8c335e38e4da8ccea3ce3bb3b991af2667808dc1
Joshua Tapia <JOSHUATAPIA777@gmail.com>
2021-12-06 15:21:59 -0800

    implemented a method to draw rectangles with various colors and positions based on a given list

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from graphics import *
+import random
+
+
+unSortedList = [5,2,8,4,10,1,3]
 
 def main():
     win = GraphWin("Graphics Window", 500,500)
@@ -6,15 +10,20 @@ def main():
     # rect = Rectangle(Point(10,100),Point(100,400))
     # rect.setFill("Red")
     # rect.draw(win)
-    createRectangles(4,win)
+    createRectangles(unSortedList,win)
     #c.draw(win)
     win.getMouse()
     win.close()
 
-def createRectangles(num, win):
-    for x in range(num):
-        rect = Rectangle(Point(10+50*x,100),Point(20+50*x,400))
-        rect.setFill("red"+str(x+1))
+def createRectangles(someList, win):
+    position = 0
+    for x in range(len(someList)):
+        print(random.randrange(3, 9))
+        position = random.randrange(10, 450, 2)
+        print(position)
+        rect = Rectangle(Point(position,100+x*20),Point(20 + position,400))
+        color = color_rgb(225 - x*20,0,0)
+        rect.setFill(color)
         rect.draw(win)
 
 main()
