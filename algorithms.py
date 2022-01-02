9bf4c39512a7e45205b45fc3fb7cec0ce8656393
Joshua Tapia <JOSHUATAPIA777@gmail.com>
2022-01-01 20:58:47 -0800

    Implemented QuickSort

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,8 @@
 unSortedList = [5,2,8,4,10,1,3]
 #0,1,2,3,4,5,8
 
+### BUBBLESORT ###
+
 #Time Complexity: Worst: O(n^2) list sorted least to greatest, Best: O(n) list already sorted
 def bubbleSort(yourList):
     for x in range(len(yourList)): # goes through the entire list
@@ -10,6 +12,8 @@ def bubbleSort(yourList):
             if(yourList[y] > yourList[y+1]): # if pointer 1 > pointer 2 swap them
                 yourList[y], yourList[y+1] = yourList[y+1], yourList[y]
     return(yourList)
+
+### INSERTIONSORT ###
 
 def insertionSort(yourList):
     for x in range(len(yourList)): # goes through the entire list
@@ -25,6 +29,8 @@ def insertionSort(yourList):
             y = y-1
             
     return(yourList)
+
+### MERGESORT ###
 
 def merge(left, right):
     print("Merging...", left, right)
@@ -53,9 +59,48 @@ def mergeSort(yourList):
         right = mergeSort(yourList[mid:]) # take the right side of the list and sort it
         return(merge(left, right))
 
+### QUICKSORT ###
+
+def partition(yourList, low, high):
+    print(yourList)
+    #the rightmost item is the pivot
+    pivot = yourList[high]
+    print("Pivot", pivot)
+
+    #pointer for holding the greater element
+    greaterIndex = low - 1
+
+    for x in range(low, high):
+        print(yourList)
+        if yourList[x] <= pivot:
+            greaterIndex += 1
+            print("Swapping", yourList[greaterIndex], yourList[x])
+            yourList[greaterIndex], yourList[x] = yourList[x], yourList[greaterIndex]
+
+    #Place pivot in sorted position
+    yourList[greaterIndex + 1], yourList[high] = yourList[high], yourList[greaterIndex + 1]
+    print("Sort Pivot", yourList)
+    return (greaterIndex + 1)
+
+def quickSort(yourList, low, high):
+    print(low, high)
+    if low < high:
+        #find the pivot point
+        pivot = partition(yourList, low, high)
+        print(pivot)
+
+        #left side of the list
+        quickSort(yourList, low, pivot - 1)
+        #right side of the list
+        quickSort(yourList, pivot + 1, high)
+    return(yourList)
+
+
+
 
 #print("Bubble Sort",bubbleSort(unSortedList))
 #print("Insertion Sort",insertionSort(unSortedList))
-print("Merge Sort",mergeSort(unSortedList))
+#print("Merge Sort",mergeSort(unSortedList))
+print("Quick Sort", quickSort(unSortedList, 0, (len(unSortedList) - 1)))
         
 
